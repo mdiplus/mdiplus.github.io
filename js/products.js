5aeaed9188dbb9413e8f7466f381e1cbf9abff0d
Max Semenenko <admin@mdibuild.com>
2016-02-24 18:02:05 -0800

    Initial upload (only files tracked with HTTrack)

---
+++ js/products.js
@@ -0,0 +1,93 @@
+window.addEvents({
+		'domready': function(){
+			/* thumbnails example , div containers */
+			new SlideItMoo({
+						overallContainer: 'Product1_outer',
+						elementScrolled: 'Product1_inner',
+						thumbsContainer: 'Product1_items',		
+						itemsVisible:5,
+						elemsSlide:3,
+						duration:125,
+						itemsSelector: '.product',
+						itemWidth: 120,
+						showControls:1,
+						startIndex:1,});
+			new SlideItMoo({
+						overallContainer: 'Product2_outer',
+						elementScrolled: 'Product2_inner',
+						thumbsContainer: 'Product2_items',		
+						itemsVisible:5,
+						elemsSlide:3,
+						duration:125,
+						itemsSelector: '.product',
+						itemWidth: 120,
+						showControls:1,
+						startIndex:1,});
+			new SlideItMoo({
+						overallContainer: 'Product3_outer',
+						elementScrolled: 'Product3_inner',
+						thumbsContainer: 'Product3_items',		
+						itemsVisible:5,
+						elemsSlide:3,
+						duration:125,
+						itemsSelector: '.product',
+						itemWidth: 120,
+						showControls:1,
+						startIndex:1,});
+			new SlideItMoo({
+						overallContainer: 'Product4_outer',
+						elementScrolled: 'Product4_inner',
+						thumbsContainer: 'Product4_items',		
+						itemsVisible:5,
+						elemsSlide:3,
+						duration:125,
+						itemsSelector: '.product',
+						itemWidth: 120,
+						showControls:1,
+						startIndex:1,});
+			new SlideItMoo({
+						overallContainer: 'Product5_outer',
+						elementScrolled: 'Product5_inner',
+						thumbsContainer: 'Product5_items',		
+						itemsVisible:5,
+						elemsSlide:3,
+						duration:125,
+						itemsSelector: '.product',
+						itemWidth: 120,
+						showControls:1,
+						startIndex:1,});
+			new SlideItMoo({
+						overallContainer: 'Product6_outer',
+						elementScrolled: 'Product6_inner',
+						thumbsContainer: 'Product6_items',		
+						itemsVisible:5,
+						elemsSlide:3,
+						duration:125,
+						itemsSelector: '.product',
+						itemWidth: 120,
+						showControls:1,
+						startIndex:1,});
+			new SlideItMoo({
+						overallContainer: 'Product7_outer',
+						elementScrolled: 'Product7_inner',
+						thumbsContainer: 'Product7_items',		
+						itemsVisible:5,
+						elemsSlide:3,
+						duration:125,
+						itemsSelector: '.product',
+						itemWidth: 120,
+						showControls:1,
+						startIndex:1,});
+			new SlideItMoo({
+						overallContainer: 'Product8_outer',
+						elementScrolled: 'Product8_inner',
+						thumbsContainer: 'Product8_items',		
+						itemsVisible:5,
+						elemsSlide:3,
+						duration:125,
+						itemsSelector: '.product',
+						itemWidth: 120,
+						showControls:1,
+						startIndex:1,});
+		}
+});
